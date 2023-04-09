ada719eefe6d44817cb5ca7502dbb12689764e28
Waris Radji <wradji64@gmail.com>
2023-04-09 22:39:41 +0200

    Rework menu animation

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function toggleMenu() {
     document.getElementById("menu-button").classList.toggle("is-active")
-    const menu = document.getElementById("menu");
+    const menu = document.getElementById("menu-container");
     if (menu.classList.contains("hidden")) {
         // Show the menu with animation
         document.body.style.overflow = 'hidden';
         anime({
             targets: menu,
             opacity: [0, 0.9],
-            height: "100vh",
-            duration: 1000,
+            //height: "100vh",
+            duration: 250,
             easing: 'easeOutQuad'
         });
         menu.classList.remove("hidden");
@@ -18,8 +18,8 @@ function toggleMenu() {
         anime({
             targets: menu,
             opacity: [0.9, 0],
-            height: 0,
-            duration: 1000,
+            //height: 0,
+            duration: 250,
             easing: 'easeInQuad',
             complete: function() {
                 menu.classList.add("hidden");
