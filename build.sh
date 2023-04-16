b4c11dfa990a9ece8cb12bf801ccfa8ec8d02044
Waris Radji <wradji64@gmail.com>
2023-04-16 03:55:02 +0200

    Update animations

--- build.sh
+++ build.sh
@@ -2,10 +2,9 @@ mkdir -p public/dist
 cp orpheus.ttf public/dist/orpheus.ttf
 touch public/.nojekyll
 npx tailwindcss -i ./src/input.css -o ./public/dist/output.css
+cp ./public/dist/output.css ./dist/output.css
 cp hamburgers.css public/dist/hamburgers.css
 cp script.js public/script.js
-cp animation.js public/animation.js
-cp animation1.js public/animation1.js
 node image_compress.js
 cp -r node_modules public/node_modules
 cd src
